a4aed71f8b7cb2e190c23c74582bc07379915a12
TuckerLandis <landistuckerc@gmail.com>
2021-06-10 14:21:40 -0500

    feature-form

--- src/components/MenuForm/MenuForm.jsx
+++ src/components/MenuForm/MenuForm.jsx
@@ -1,6 +1,28 @@
+import { useSelector } from "react-redux"
+import MenuItem from './MenuItem/MenuItem'
+
+function MenuForm () {
+
+    const menu = useSelector(store => store.menuReducer)
+
+    return (
+        <div>
+            <div className="display">
+          
+            {menu.map((item) => 
+          <MenuItem key={item.id} />
+        )}
+
+            </div>
+
+            <button>Next</button>
+
+        </div>
+
+    )
+}
 
 
 
 
-
-//export default MenuForm
+export default MenuForm
