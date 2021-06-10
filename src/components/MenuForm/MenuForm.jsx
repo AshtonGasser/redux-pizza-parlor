7470e1ca108113295d1854415aa08a05635bcaf8
TuckerLandis <landistuckerc@gmail.com>
2021-06-10 14:36:04 -0500

    big string pizza blast

--- src/components/MenuForm/MenuForm.jsx
+++ src/components/MenuForm/MenuForm.jsx
@@ -1,9 +1,51 @@
-import { useSelector } from "react-redux"
-import MenuItem from './MenuItem/MenuItem'
+import { useEffect } from "react"
+import { useSelector,  useDispatch } from "react-redux"
+import MenuItem from '../MenuItem/MenuItem'
+import axios from 'axios'
+
 
 function MenuForm () {
-
+    const dispatch = useDispatch()
     const menu = useSelector(store => store.menuReducer)
+
+      // get Artists data from server on load
+  useEffect(() => {
+    console.log('in useEffect');
+    setMenu();
+    getMenu();
+  }, []);
+
+const setMenu = () => {
+    axios.get('/api/pizza')
+    .then( response => {
+        dispatch({
+            type: 'SET_MENU',
+            payload: response.data
+          })
+    })
+    .catch(error => {
+        console.log('error getting menu', error);
+        
+    })     
+
+}
+
+const getMenu = () => {
+    axios.get('/api/pizza')
+    .then( response => {
+        dispatch({
+            type: 'GET_MENU',
+            
+          })
+    })
+    .catch(error => {
+        console.log('error getting menu', error);
+        
+    })     
+
+}
+
+
 
     return (
         <div>
